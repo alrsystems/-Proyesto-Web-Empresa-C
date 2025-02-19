2ab32cc52ceb7cba3c2646151a7787b307743568
AlrSystems <alfredbayon@hotmail.com>
2025-02-19 19:05:11 +0100

    Paractica de javascript pruebas

--- JavaScript.js
+++ JavaScript.js
@@ -1,9 +1,9 @@
 // Archivo: script.js
 
-// Función para mostrar un mensaje de saludo
-function mostrarSaludo() {
+// Función para mostrar un mensaje de lo que sea
+function mostrarMensaje() {
     alert("¡Hola! Bienvenido a mi página web.");
 }
 
 // Llamar a la función cuando se carga la página
-window.onload = mostrarSaludo;
+//gitqwindow.onload = mostrarMensaje;
